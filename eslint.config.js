031043562bdf617299f4d5d2be06e597edd6584a
sence1-nathansaludes <nathaniel.saludes@sence1.com>
2024-04-23 02:38:00 +0800

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  ...eslintPluginAstro.configs['flat/recommended'],
+  eslintConfigPrettier,
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      eqeqeq: 'error',
+    },
+  },
+];
